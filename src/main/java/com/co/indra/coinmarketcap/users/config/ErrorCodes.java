aa339241d8b667148893a4f3569545cb423ccd56
titan9804 <jaimeandresmarinalarcon@gmail.com>
2022-05-16 12:48:07 -0500

    Update ErrorCodes.java

--- src/main/java/com/co/indra/coinmarketcap/users/config/ErrorCodes.java
+++ src/main/java/com/co/indra/coinmarketcap/users/config/ErrorCodes.java
@@ -15,15 +15,9 @@ public enum ErrorCodes {
         return message;
     }
 
-    //public void setMessage(String message) {
-       // this.message = message;
-    //}
-
     public String getCode() {
         return code;
     }
 
-   // public void setCode(String code) {
-      //  this.code = code;
-    //}
+
 }
