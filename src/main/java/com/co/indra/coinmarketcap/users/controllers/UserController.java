84c2a09e565c6d6e97bf8e89dbda0afe60ded675
juan manuel <jmcontrerasvega@gmail.com>
2022-06-13 07:45:45 -0500

    juanManuel/updated

--- src/main/java/com/co/indra/coinmarketcap/users/controllers/UserController.java
+++ src/main/java/com/co/indra/coinmarketcap/users/controllers/UserController.java
@@ -1,6 +1,5 @@
 package com.co.indra.coinmarketcap.users.controllers;
 
-
 import com.co.indra.coinmarketcap.users.config.Routes;
 import com.co.indra.coinmarketcap.users.model.User;
 import com.co.indra.coinmarketcap.users.services.UserService;
@@ -12,16 +11,17 @@ import java.util.List;
 @RestController
 @RequestMapping(Routes.USERS_PATH)
 public class UserController {
-    @Autowired
-    private UserService userService;
-    @PostMapping
-    public void create(@RequestBody User user) {
-        userService.createUser(user);
-    }
+	@Autowired
+	private UserService userService;
 
-    @GetMapping(Routes.USER_PATH_PARAM)
-    public List<User> getUser(@PathVariable("idUser") int idUser){
-        return userService.getUsers(idUser);
-    }
+	@PostMapping
+	public void create(@RequestBody User user) {
+		userService.createUser(user);
+	}
+
+	@GetMapping(Routes.USER_PATH_PARAM)
+	public User getUser(@PathVariable("idUser") int idUser) {
+		return userService.getUsers(idUser);
+	}
 
 }
